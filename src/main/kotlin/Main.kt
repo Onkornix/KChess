8fec01e4e995cc6e982cef32bb740d181e189992
onkornix <dymyers@icloud.com>
2023-10-27 21:43:32 -0700

    maybe this is gonna work I dunno this sucks

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,18 @@
+/*
+create a coordinate map (0,0)..(8,8)
+each piece is a class:
+    contains moves, position, capture conditions?
+
+
+
+ */
+
 
 
 fun main() {
-    val board = listOf(
-        listOf( Pawn().at("a_8"),
-        )
-    )
+    val board = Board()
+    board.setupBoard()
+    board.printBoard()
 
+    board.pawnJerry.move(7,0)
 }
--- src/main/kotlin/Pawn.kt
+++ src/main/kotlin/Pawn.kt
@@ -1,9 +1,26 @@
 class Pawn {
-    init {
+    var position: List<Int> = listOf()
+    var nearbyPieces: List<List<Int>> = listOf()
+    var firstMoveUsed = false
+    fun at(posx: Int, posy: Int): List<Int> {
+        position = listOf(posx,posy)
+        return position
+    }
+    fun move(posx: Int, posy: Int) {
+        /*
+        can move y+1 unless firstMovedUsed = false, then move y+2,
+        or a piece is within (x+/-1,y+/-1) then move (x+/-1,y+/-1)
+        and capture piece
+         */
+
+
+
+    }
+    fun findNearbyPieces() {
+
+    }
+    fun checkIllegal() {
 
     }
 
-    fun at(pos: String){
-
-    }
 }
